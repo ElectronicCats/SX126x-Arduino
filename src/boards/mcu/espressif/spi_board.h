abf35ef317f51a21ab5d84471d1c7c4f61d84aed
sabas1080 <sabasjimenez@gmail.com>
2021-02-07 17:12:35 -0600

    remove unnecesary files esp

--- src/boards/mcu/espressif/spi_board.h
+++
@@ -1,8 +0,0 @@
-#ifndef SPI_BOARD_H
-#define SPI_BOARD_H
-#include <SPI.h>
-
-extern SPIClass SPI;
-
-void initSPI(void);
-#endif // SPI_BOARD_H
--- src/boards/mcu/spi_board.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SPI_BOARD_H
-#define _SPI_BOARD_H
-#if defined ESP8266 || defined ESP32
-#include "boards/mcu/espressif/spi_board.h"
-#endif
-#endif
